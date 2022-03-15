d808999417f769072b370d6d142383b5d3a48c9f
ambareeshns <101534947+ambareeshns@users.noreply.github.com>
2022-03-15 14:35:53 +0530

    Delete mail.sh

--- mail.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-size=`df -h . | awk '(NR>1) {print $(NF-1)}' | sed 's/%/ /g'`
-if [ $size -gt 20 ]
-then
-	echo "memory reaches threshold value" | mail -s "disk usage" ambareeshns@gmail.com
-fi
